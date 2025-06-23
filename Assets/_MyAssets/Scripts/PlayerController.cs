066fbffcbf4dd62e592ee2dd4cc9ef403a337066
geetttoo7 <gitpianoofficial@gmail.com>
2025-06-23 10:21:54 +0530

    Minor Updates

--- Assets/_MyAssets/Scripts/PlayerController.cs
+++ Assets/_MyAssets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
+    public InputType inputType;
 
     GameManager gameManager;
     Gyroscope gyro;
@@ -26,26 +27,36 @@ public class PlayerController : MonoBehaviour
     {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-#if UNITY_EDITOR
-        
-        if (Input.GetKey("d"))
-            rb.velocity = new Vector3(sidewaysForce, rb.velocity.y, rb.velocity.z);
-        else if (Input.GetKey("a"))
-            rb.velocity = new Vector3(-sidewaysForce, rb.velocity.y, rb.velocity.z);
-        else
-            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
-#else
-       
-        if (gyroSupported)
+        switch (inputType)
         {
-            float tilt = gyro.gravity.x; // adjust axis if needed
-            rb.velocity = new Vector3(tilt * sidewaysForce * 2f, rb.velocity.y, rb.velocity.z);
-        }
-#endif
+            case InputType.KEYBOARD_MOUSE:
+                if (Input.GetKey("d"))
+                    rb.velocity = new Vector3(sidewaysForce, rb.velocity.y, rb.velocity.z);
+                else if (Input.GetKey("a"))
+                    rb.velocity = new Vector3(-sidewaysForce, rb.velocity.y, rb.velocity.z);
+                else
+                    rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+                break;
+            case InputType.GYRO:
+                if (gyroSupported)
+                {
+                    float tilt = gyro.gravity.x; // adjust axis if needed
+                    rb.velocity = new Vector3(tilt * sidewaysForce * 2f, rb.velocity.y, rb.velocity.z);
+                }
 
+                break;
+        }
+     
         if (rb.position.y < -1f)
         {
             gameManager.EndGame();
         }
     }
+}
+
+public enum InputType
+{
+    KEYBOARD_MOUSE,
+    GYRO,
+    TOUCH
 }
